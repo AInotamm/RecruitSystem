36757e507bfb21477b216feb5fb4abf707a13fad
noFloat <304546210@qq.com>
2015-09-07 01:06:04 +0800

    组织成员init

---
+++ Application/Home/Model/UsersorganisationModel.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Model;
+use Think\Model;
+class UsersorganisationModel extends Model {
+	private $_users_organisation;
+	public function setUO(){
+		$this->_users_organisation = M('usersorganisation');
+	}
+
+	public function checkUO($condition,$key){
+		$this->setUO();
+		$UO = $this->_users_organisation->where($condition)->field($key)->select();
+		return $UO;
+	}
+
+}
